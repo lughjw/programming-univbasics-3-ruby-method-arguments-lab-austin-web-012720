0a8ca81663f0a83df60136a89ca4856b3a28d53b
Lugh Wilson <lughjw@gmail.com>
2019-12-20 21:46:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,7 @@ def introduction_with_language_optional(name, language)
   if language != false
     introduction_with_language(name,language)
   else
-    
+    introduction_with_language(name, "Ruby")
 end
 
 
